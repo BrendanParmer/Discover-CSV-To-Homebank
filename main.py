396af70109be6ac8e61dd16a5c6e74c15b510d35
BrendanParmer <51296046+BrendanParmer@users.noreply.github.com>
2023-11-11 14:07:02 -0600

    feat: initial script

---
+++ main.py
@@ -0,0 +1,34 @@
+import argparse
+import pandas as pd
+
+def transform_csv(input_file, output_file):
+    # Load your CSV data into a pandas DataFrame
+    credit_card_data = pd.read_csv(input_file)
+
+    # For some reason Discover thought it'd be a good idea to list all the
+    # credits as negative and the debits as positive
+    credit_card_data['Amount'] *= -1
+
+    # Create a new DataFrame with the desired columns and transform the data
+    transformed_data = pd.DataFrame({
+        'date': credit_card_data['Trans. Date'],
+        'payment': 1,
+        'info': '',
+        'payee': '',
+        'memo': credit_card_data['Description'],
+        'amount': credit_card_data['Amount'],
+        'category': credit_card_data['Category'],
+        'tags': ''
+    })
+
+    # Save the transformed data to a new CSV file
+    transformed_data.to_csv(output_file, index=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Transform credit card CSV data for financial software.')
+    parser.add_argument('input_file', help='Input CSV file (credit card data)')
+    parser.add_argument('output_file', help='Output CSV file (transformed data)')
+
+    args = parser.parse_args()
+
+    transform_csv(args.input_file, args.output_file)
